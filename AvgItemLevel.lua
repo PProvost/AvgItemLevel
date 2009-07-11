144525459b12565910cb67780cbc37cbf16794f8
PProvost <peter@provost.org>
2009-07-11 14:48:43 -0600

    Updated to conform to current scaling for non-epic items

--- AvgItemLevel.lua
+++ AvgItemLevel.lua
@@ -14,9 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ]]
 
-local slots = { "AmmoSlot", "BackSlot", "ChestSlot", "FeetSlot", "Finger0Slot", "Finger1Slot", "HandsSlot", "HeadSlot", "LegsSlot", "MainHandSlot", "NeckSlot", "RangedSlot", "SecondaryHandSlot", "ShoulderSlot", "Trinket0Slot", "Trinket1Slot", "WaistSlot", "WristSlot" }
+local slots = { "BackSlot", "ChestSlot", "FeetSlot", "Finger0Slot", "Finger1Slot", "HandsSlot", "HeadSlot", "LegsSlot", "MainHandSlot", "NeckSlot", "ShoulderSlot", "Trinket0Slot", "Trinket1Slot", "WaistSlot", "WristSlot" }
 
--- Event hander frame
 local f = CreateFrame("frame")
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 f:RegisterEvent("ADDON_LOADED")
@@ -29,29 +28,31 @@ function f:ADDON_LOADED(event, addon)
 end
 
 function f:PLAYER_LOGIN()
-	self:RegisterEvent("PLAYER_LOGOUT")
-
-	-- Setup fontstring on the PaperDollFrame
 	self.fontString = PaperDollFrame:CreateFontString("AvgItemLevelString", "ARTWORK", "GameFontNormalSmall")
 	self.fontString:SetPoint("BOTTOMRIGHT", PaperDollFrame, "BOTTOMRIGHT", -50, 86)
 	PaperDollFrame:HookScript("OnShow", function(self) f:Calculate() end)
-
-	self:UnregisterEvent("PLAYER_LOGIN")
-	self.PLAYER_LOGIN = nil
+	self:UnregisterEvent("PLAYER_LOGIN"); self.PLAYER_LOGIN = nil
 end
 
 function f:Calculate()
-	local total, count = 0, 0
-	local slot, link, _, iLevel
+	local total = 0
+	local slot, link, quality, _, iLevel
 	for i = 1,#slots do
+		iLevel = 0
 		slot = GetInventorySlotInfo(slots[i])
 		link = GetInventoryItemLink("player", slot)
 		if link then
-			iLevel = select(4, GetItemInfo(link))
-			total = total + (iLevel or 0)
-			count = count + 1
+			_, _, quality, iLevel = GetItemInfo(link)
+
+			-- Assumptions: 
+			--    Artifact/heirloom (yellow) and legendary (orange) are the same as Epic (purple)
+			--    Common (white) and Poor (gray) are 0
+			if quality = 4 then iLevel = iLevel - 13 end
+			if quality = 3 then iLevel = iLevel - 26 end
+			if quality < 3 then iLevel = 0 end
 		end
+		total = total + (iLevel or 0)
 	end
 
-	self.fontString:SetText( string.format("%.2f", total/count) )
+	self.fontString:SetText( string.format("%.2f", total/#slots) )
 end
