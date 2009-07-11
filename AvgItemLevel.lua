d30d161edc67a0594c956fa1c9982b312dd4c714
PProvost <peter@provost.org>
2009-07-11 14:24:27 -0600

    Basic "player" implementation copied over from my hack script. Seems to work so far.

--- AvgItemLevel.lua
+++ AvgItemLevel.lua
@@ -1,71 +1,57 @@
+--[[
+Copyright 2009 Quaiche of Dragonblight
 
-----------------------
---      Locals      --
-----------------------
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-local L = setmetatable({}, {__index=function(t,i) return i end})
-local defaults, defaultsPC, db, dbpc = {}, {}
+http://www.apache.org/licenses/LICENSE-2.0
 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
 
-------------------------------
---      Util Functions      --
-------------------------------
+local slots = { "AmmoSlot", "BackSlot", "ChestSlot", "FeetSlot", "Finger0Slot", "Finger1Slot", "HandsSlot", "HeadSlot", "LegsSlot", "MainHandSlot", "NeckSlot", "RangedSlot", "SecondaryHandSlot", "ShoulderSlot", "Trinket0Slot", "Trinket1Slot", "WaistSlot", "WristSlot" }
 
-local function Print(...) print("|cFF33FF99AvgItemLevel|r:", ...) end
-
-local debugf = tekDebug and tekDebug:GetFrame("AvgItemLevel")
-local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
-
-
------------------------------
---      Event Handler      --
------------------------------
-
+-- Event hander frame
 local f = CreateFrame("frame")
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 f:RegisterEvent("ADDON_LOADED")
 
-
 function f:ADDON_LOADED(event, addon)
 	if addon:lower() ~= "avgitemlevel" then return end
-
-	AvgItemLevelDB, AvgItemLevelDBPC = setmetatable(AvgItemLevelDB or {}, {__index = defaults}), setmetatable(AvgItemLevelDBPC or {}, {__index = defaultsPC})
-	db, dbpc = AvgItemLevelDB, AvgItemLevelDBPC
-
-	-- Do anything you need to do after addon has loaded
-
-	LibStub("tekKonfig-AboutPanel").new("AvgItemLevel", "AvgItemLevel") -- Make first arg nil if no parent config panel
-
-	self:UnregisterEvent("ADDON_LOADED")
-	self.ADDON_LOADED = nil
-
+	LibStub("tekKonfig-AboutPanel").new(nil, "AvgItemLevel")
+	self:UnregisterEvent("ADDON_LOADED"); self.ADDON_LOADED = nil
 	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
 end
-
 
 function f:PLAYER_LOGIN()
 	self:RegisterEvent("PLAYER_LOGOUT")
 
-	-- Do anything you need to do after the player has entered the world
+	-- Setup fontstring on the PaperDollFrame
+	self.fontString = PaperDollFrame:CreateFontString("AvgItemLevelString", "ARTWORK", "GameFontNormalSmall")
+	self.fontString:SetPoint("BOTTOMRIGHT", PaperDollFrame, "BOTTOMRIGHT", -50, 86)
+	PaperDollFrame:HookScript("OnShow", function(self) f:Calculate() end)
 
 	self:UnregisterEvent("PLAYER_LOGIN")
 	self.PLAYER_LOGIN = nil
 end
 
+function f:Calculate()
+	local total, count = 0, 0
+	local slot, link, _, iLevel
+	for i = 1,#slots do
+		slot = GetInventorySlotInfo(slots[i])
+		link = GetInventoryItemLink("player", slot)
+		if link then
+			iLevel = select(4, GetItemInfo(link))
+			total = total + (iLevel or 0)
+			count = count + 1
+		end
+	end
 
-function f:PLAYER_LOGOUT()
-	for i,v in pairs(defaults) do if db[i] == v then db[i] = nil end end
-	for i,v in pairs(defaultsPC) do if dbpc[i] == v then dbpc[i] = nil end end
-
-	-- Do anything you need to do as the player logs out
-end
-
-
------------------------------
---      Slash Handler      --
------------------------------
-
-SLASH_ADDONTEMPLATE1 = "/avgitemlevel"
-SlashCmdList.ADDONTEMPLATE = function(msg)
-	-- Do crap here
+	self.fontString:SetText( string.format("%.2f", total/count) )
 end
